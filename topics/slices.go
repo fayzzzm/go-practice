15ff232da2cdce3c79b0c054fda3c199de8924c2
fayzzzm <m.fayzov1@gmail.com>
2024-05-13 17:25:20 +0500

    added some extra implementation with range

--- topics/slices.go
+++ topics/slices.go
@@ -6,16 +6,16 @@ func main() {
 	var a [10]int
 
 	b := [5]int{1, 2, 3, 4, 5}
-	c := [5]int{1, 3: 4, 5}
-	d := [10]int{1, 3: 4, 5, 8: 9}
+	// c := [5]int{1, 3: 4, 5}
+	// d := [10]int{1, 3: 4, 5, 8: 9}
 
 	a[0] = 1
 	a[1] = 2
 
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+	// fmt.Println(d)
 
 	var dp [5][5]int
 
@@ -23,11 +23,15 @@ func main() {
 	dp[0][1] = 2
 	dp[4][4] = 3
 
-	s := b[2:5]
-	f := d[3:]
+	// s := b[2:5]
+	// f := d[3:]
 
-	defer fmt.Println(s)
-	defer fmt.Println(f)
+	// defer fmt.Println(s)
+	// defer fmt.Println(f)
 
-	fmt.Println(dp)
+	for item, index := range b {
+		fmt.Println(item, index)
+	}
+
+	// fmt.Println(dp)
 }
